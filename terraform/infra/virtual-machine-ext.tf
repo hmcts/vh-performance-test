0d441505ff468fbc88b6bdf7845f43a133875256
Benjamin Garside (v1) <114483383+benjamin-v1@users.noreply.github.com>
2022-10-13 14:43:51 +0100

    Vih 9405 install jmeter (#2)
    * adding extention
    
    * fix formatting
    
    * adding tags
    
    * fix validation
    
    * ADDING DTS SDS Developers AD Group
    
    * removing pr trigger

---
+++ terraform/infra/virtual-machine-ext.tf
@@ -0,0 +1,22 @@
+
+locals {
+  winscript = "powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) ;powershell choco install jmeter --ignore-checksums -y ; powershell exit 0"
+}
+
+resource "azurerm_virtual_machine_extension" "perf_test" {
+  count = var.vm_instance_count
+
+  name                 = "chocoInstall-${count.index + 1}"
+  virtual_machine_id   = azurerm_windows_virtual_machine.perf_test[count.index].id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  protected_settings = <<PSETTINGS
+    {
+       "commandToExecute": "${local.winscript}"
+    }
+PSETTINGS
+
+  tags = local.common_tags
+}
