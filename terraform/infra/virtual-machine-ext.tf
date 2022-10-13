17e6618d9bfce3e6200495ab288e07c9ee5a841d
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-13 13:11:26 +0100

    fix formatting

--- terraform/infra/virtual-machine-ext.tf
+++ terraform/infra/virtual-machine-ext.tf
@@ -1,7 +1,7 @@
 
 locals {
   winscript = "powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) ;powershell choco install jmeter --ignore-checksums -y ; powershell exit 0"
-}                          
+}
 
 resource "azurerm_virtual_machine_extension" "perf_test" {
   count = var.vm_instance_count
