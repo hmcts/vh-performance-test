e45179fb1be992be9ee43e9dfdb8d4a5a3f3dd1d
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-13 10:33:54 +0100

    add access and chnage vm size

--- terraform/infra/key-vault.tf
+++ terraform/infra/key-vault.tf
@@ -16,3 +16,75 @@ resource "azurerm_key_vault" "perf_test" {
   tags = local.common_tags
 
 }
+
+resource "azurerm_key_vault_access_policy" "dts_operations" {
+  key_vault_id = azurerm_key_vault.perf_test.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  certificate_permissions = [
+    "Backup",
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "ManageContacts",
+    "ManageIssuers",
+    "Purge",
+    "Recover",
+    "Restore",
+    "SetIssuers",
+    "Update"
+  ]
+
+  key_permissions = [
+    "Backup",
+    "Create",
+    "Decrypt",
+    "Delete",
+    "Encrypt",
+    "Get",
+    "Import",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Sign",
+    "UnwrapKey",
+    "Update",
+    "Verify",
+    "WrapKey"
+  ]
+
+  secret_permissions = [
+    "Backup",
+    "Delete",
+    "Get",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Set"
+  ]
+
+  storage_permissions = [
+    "Backup",
+    "Delete",
+    "DeleteSAS",
+    "Get",
+    "GetSAS",
+    "List",
+    "ListSAS",
+    "Purge",
+    "Recover",
+    "RegenerateKey",
+    "Restore",
+    "Set",
+    "SetSAS",
+    "Update"
+  ]
+}
--- terraform/vars/dev.tfvars
+++ terraform/vars/dev.tfvars
@@ -2,5 +2,5 @@
 
 location          = "UK South"
 address_space     = ["10.100.198.64/28"]
-vm_size           = "Standard_D8_v3"
+vm_size           = "Standard_D8_v5"
 vm_instance_count = 2
