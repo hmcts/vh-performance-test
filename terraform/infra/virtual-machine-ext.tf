90db439d385254eb004163db3dce453c13ea09c8
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-20 11:35:31 +0100

    changing type_handler_version

--- terraform/infra/virtual-machine-ext.tf
+++ terraform/infra/virtual-machine-ext.tf
@@ -33,7 +33,7 @@ resource "azurerm_virtual_machine_extension" "AADLoginForWindows" {
   virtual_machine_id         = azurerm_windows_virtual_machine.perf_test[count.index].id
   publisher                  = "Microsoft.Azure.ActiveDirectory"
   type                       = "AADLoginForWindows"
-  type_handler_version       = "1.0.0.1"
+  type_handler_version       = "1.0"
   auto_upgrade_minor_version = true
 
 }
