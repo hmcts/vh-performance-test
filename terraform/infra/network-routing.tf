a4166709b75a38279819193dec4e33d9fb44777d
Your Name <james.ferrari@version1.com>
2022-11-01 12:09:33 +0000

    remove routes back, managed elsewhere

--- terraform/infra/network-routing.tf
+++ terraform/infra/network-routing.tf
@@ -21,19 +21,3 @@ resource "azurerm_subnet_route_table_association" "perf_test" {
   subnet_id      = azurerm_subnet.perf_test.id
   route_table_id = azurerm_route_table.perf_test.id
 }
-
-data "azurerm_route_table" "aks_appgw_route_table" {
-  count               = var.environment == "stg" ? 1 : 0
-  name                = "aks-${var.environment}-appgw-route-table"
-  resource_group_name = "ss-${var.environment}-network-rg"
-}
-
-resource "azurerm_route" "vh_perf_test" {
-  count                  = var.environment == "stg" ? 1 : 0
-  name                   = local.service_name
-  resource_group_name    = data.azurerm_route_table.aks_appgw_route_table[0].resource_group_name
-  route_table_name       = data.azurerm_route_table.aks_appgw_route_table[0].name
-  address_prefix         = element(var.address_space, 0)
-  next_hop_type          = "VirtualAppliance"
-  next_hop_in_ip_address = "10.11.8.36"
-}
