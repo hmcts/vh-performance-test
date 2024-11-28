498fe4b9aa2f442544a80b4546f13fa5ad9e2586
Sam Kompfner <sam.kompfner@hmcts.net>
2024-11-28 09:01:04 +0000

    update property

--- terraform/infra/network-routing.tf
+++ terraform/infra/network-routing.tf
@@ -2,7 +2,7 @@ resource "azurerm_route_table" "perf_test" {
   name                          = local.service_name
   location                      = azurerm_resource_group.perf_test.location
   resource_group_name           = azurerm_resource_group.perf_test.name
-  disable_bgp_route_propagation = false
+  bgp_route_propagation_enabled = true
 
   tags = local.common_tags
 
